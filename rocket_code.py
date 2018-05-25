0c09b3d8997046a90ee4a217a5ddfd8607a5c744
Olivia <olivia.bear@icloud.com>
2018-05-25 18:54:56 +0000

    with camera

--- rocket_code.py
+++ rocket_code.py
@@ -1,8 +1,19 @@
 #rocket code
-
 from L3GD20 import L3GD20
-
+import RPi.GPIO as GPIO
+from picamera import PiCamera
+from time import sleep
 import time
+from datetime import date
+from datetime import time
+from datetime import datetime
+camera = PiCamera()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(6,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.output(6,GPIO.HIGH)
 
 # Communication object
 s = L3GD20(busId = 1, slaveAddr = 0x6b, ifLog = False, ifWriteBlock=False)
@@ -14,6 +25,10 @@ s.Set_AxisX_Enabled(True)
 s.Set_AxisY_Enabled(True)
 s.Set_AxisZ_Enabled(True)
 
+time.sleep(10)
+counter = 0
+
+GPIO.output(17,GPIO.HIGH)
 # Print current configuration
 s.Init()
 s.Calibrate()
@@ -23,14 +38,23 @@ dt = 0.02
 x = 0
 y = 0
 z = 0
-while 1==1:
-	time.sleep(dt)
-	dxyz = s.Get_CalOut_Value()
-	x += dxyz[0]*dt;
-	y += dxyz[1]*dt;
-	z += dxyz[2]*dt;
-	print("{:7.2f} {:7.2f} {:7.2f}".format(x, y, z))
+camera.start_preview(fullscreen=False,window=(100,200,300,400))
+camera.start_recording("/home/pi/rocketvids/test.h264")
+while counter < 20:
+        
+        time.sleep(dt)
+        dxyz = s.Get_CalOut_Value()
+        x += dxyz[0]*dt;
+        y += dxyz[1]*dt;
+        z += dxyz[2]*dt;
+        print("{:7.2f} {:7.2f} {:7.2f}".format(x, y, z))
 
+        time.sleep(.5)
+        counter = counter + 1
+camera.stop_recording()
+camera.stop_preview()
+GPIO.output(6,GPIO.LOW)
+GPIO.output(17,GPIO.LOW)
 
 
 # here is some new stuff.  cool.
